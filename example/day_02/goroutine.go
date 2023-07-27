45a1102679a7781f6093cf6bb6465642d8405e78
Turbo-King <1362371739@qq.com>
2023-07-27 11:13:16 +0800

    :sparkles:  进度：完成web案例

--- example/day_02/goroutine.go
+++ example/day_02/goroutine.go
@@ -1,1 +1,18 @@
-package day_02
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func hello(i int) {
+	fmt.Println("hello goroutine:" + fmt.Sprint(i))
+}
+func main() {
+	for i := 0; i < 5; i++ {
+		go func(j int) {
+			hello(j)
+		}(i)
+	}
+	time.Sleep(time.Second)
+}
--- example/day_02/webServer/concurrence/channel.go
+++ example/day_02/webServer/concurrence/channel.go
@@ -1,32 +1,21 @@
-package main
-
-import "fmt"
+package concurrence
 
 func CalSquare() {
 	src := make(chan int)
 	dest := make(chan int, 3)
-
-	// 生产者
 	go func() {
 		defer close(src)
 		for i := 0; i < 10; i++ {
 			src <- i
 		}
 	}()
-
-	// 消费者
 	go func() {
 		defer close(dest)
 		for i := range src {
 			dest <- i * i
 		}
 	}()
-
 	for i := range dest {
-		fmt.Println(i)
+		println(i)
 	}
-}
-
-func main() {
-	CalSquare()
 }
